a1172e324fdb2a15df50e181963c8ba9c6cde1bb
AnhNguynTuan <nguyentuan27082004@gmail.com>
2024-05-19 06:37:25 +0700

    tuan2 bai2 Nguyentuan a46337

---
+++ app/src/main/java/com/example/ltudddtuan2bai2/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.ltudddtuan2bai2;
+
+import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    ListView lv1;
+    String sanpham[] = {"Nokia", "Samsung", "Iphone", "XiaoMi", "Sony"};
+    ArrayAdapter<String> myadapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        lv1 = findViewById(R.id.lv1);
+        myadapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, sanpham);
+        lv1.setAdapter(myadapter);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
